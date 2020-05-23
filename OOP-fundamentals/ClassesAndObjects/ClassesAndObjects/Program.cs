b7a58d22508f81feeaf91bb0acc2242130e72ba6
Thiago Cruz <thagocruz@gmail.com>
2020-05-23 17:04:20 -0300

    Add first chapter classes and objects

---
+++ OOP-fundamentals/ClassesAndObjects/ClassesAndObjects/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.Name = "Jessica";
+            Console.WriteLine(person1.Name);
+        }
+
+        class Person
+        {
+            public string Name;
+            public int Age;
+            public string Gender;
+        }
+    }
+}
