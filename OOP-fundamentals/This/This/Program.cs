1b7886b65b25a74c01764f3746d63b18c989c771
Thiago Cruz <thagocruz@gmail.com>
2020-06-02 00:50:05 -0300

    Add final this example

--- OOP-fundamentals/This/This/Program.cs
+++ OOP-fundamentals/This/This/Program.cs
@@ -7,10 +7,35 @@ namespace This
         public int MaxTemperature { get; set; }
         public int MinTemperature { get; set; }
 
-        public Weather(int MaxTemperature, int MinTemperature)
+        public Weather(RioDeJaneiroWeather weather)
         {
-            this.MaxTemperature = MaxTemperature;
-            this.MinTemperature = MinTemperature;
+            this.MaxTemperature = weather.MaxTemperature;
+            this.MinTemperature = weather.MinTemperature;
+
+            Console.WriteLine($"Max/Min temperature:{this.MaxTemperature}/{this.MinTemperature}");
+        }
+    }
+
+    class RioDeJaneiroWeather
+    {
+        public int MaxTemperature { get; set; }
+        public int MinTemperature { get; set; }
+
+        public RioDeJaneiroWeather(int maxTemperature, int minTemperature)
+        {
+            MaxTemperature = maxTemperature;
+            MinTemperature = minTemperature;
+
+            Weather weather = new Weather(this);
+            Console.WriteLine($"Max/Min temperature:{weather.MaxTemperature}/{weather.MinTemperature}");
+        }
+    }
+
+    class WeatherFinal
+    {
+        static void Main(string[] args)
+        {
+
         }
     }
 }
