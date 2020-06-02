da8060956e80b3eed0e4017f4d0ac6ff7f1d53bd
Thiago Cruz <thagocruz@gmail.com>
2020-06-02 00:26:41 -0300

    Add this sample

---
+++ OOP-fundamentals/This/This/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace This
+{
+    class Weather
+    {
+        public int MaxTemperature { get; set; }
+        public int MinTemperature { get; set; }
+
+        public Weather(int MaxTemperature, int MinTemperature)
+        {
+            this.MaxTemperature = MaxTemperature;
+            this.MinTemperature = MinTemperature;
+        }
+    }
+}
