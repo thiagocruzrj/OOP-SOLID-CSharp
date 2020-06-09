1c134bd1066e125b511a7fc3941643d3dd0213fe
Thiago Cruz <thagocruz@gmail.com>
2020-06-09 00:29:51 -0300

    Add reference and program classes

---
+++ OOP-fundamentals/AccessModifiers/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args){
+            
+        }
+    }
+}
---
+++ OOP-fundamentals/AccessModifiers2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AccessModifiers2
+{
+    class Program
+    {
+        static void Main(string[] args){
+            
+        }
+    }
+}
