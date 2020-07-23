4fb531e887e2f378cd05815c78285d00e79f5447
Thiago Cruz <thagocruz@gmail.com>
2020-07-23 00:10:38 -0300

    Add wrong example

---
+++ Solid/1 - SRP/WrongSample/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace WrongSample
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public void Add()
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(Name)) throw new ArgumentException(Name);
+                if(string.IsNullOrEmpty(Description)) throw new ArgumentException(Description);
+
+                using(var conn = new SqlConnection())
+                {
+                    var command = new SqlCommand
+                    {
+                        CommandText = "INSERT INTO PRODUCT(Name, Description) VALUES (@name, @description)"
+                    };
+                    command.Parameters.AddWithValue("name", Name);
+                    command.Parameters.AddWithValue("description", Description);
+                    conn.Open();
+                    command.ExecuteNonQuery();
+                    conn.Close();
+                }
+            } 
+            catch (Exception ex)
+            {
+                System.IO.File.WriteAllText(@"c:\Error.txt", ex.ToString());
+                throw ex;
+            }
+        }
+    }
+}
